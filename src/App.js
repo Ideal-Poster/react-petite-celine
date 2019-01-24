8376c5deb53391e93ee3562838ae5b4461f30594
Malcolm Gourdine <malcolmgourdine@gmail.com>
2019-01-24 01:03:00 -0500

    music player added

--- src/App.js
+++ src/App.js
@@ -2,24 +2,57 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import MusicPlayer from './music-player/MusicPlayer';
+
+const playlist = [
+  {
+    url: 'http://res.cloudinary.com/alick/video/upload/v1502689683/Luis_Fonsi_-_Despacito_ft._Daddy_Yankee_uyvqw9.mp3',
+    cover: 'http://res.cloudinary.com/alick/image/upload/v1502689731/Despacito_uvolhp.jpg',
+    title: 'Despacito',
+    artist: [
+      'Luis Fonsi',
+      'Daddy Yankee'
+    ]
+  },
+  {
+    url: 'http://res.cloudinary.com/alick/video/upload/v1502375674/Bedtime_Stories.mp3',
+    cover: 'http://res.cloudinary.com/alick/image/upload/v1502375978/bedtime_stories_bywggz.jpg',
+    title: 'Bedtime Stories',
+    artist: [
+      'Jay Chou'
+    ]
+  },
+  {
+    url: 'http://res.cloudinary.com/alick/video/upload/v1502444212/Actor_ud8ccw.mp3',
+    cover: 'http://res.cloudinary.com/alick/image/upload/v1502444304/actor_umzdur.jpg',
+    title: '演员',
+    artist: [
+      '薛之谦'
+    ]
+  },
+  {
+    url: 'http://res.cloudinary.com/alick/video/upload/v1502444215/Bridge_of_Fate_aaksg1.mp3',
+    cover: 'http://res.cloudinary.com/alick/image/upload/v1502444306/Bridge_of_Fate_o36rem.jpg',
+    title: 'Bridge of Fate',
+    artist: [
+      '王力宏',
+      '谭维维'
+    ]
+  },
+  {
+    url: 'http://res.cloudinary.com/alick/video/upload/v1502444222/Goodbye_byaom5.mp3',
+    cover: 'http://res.cloudinary.com/alick/image/upload/v1502444310/Goodbye_hpubmk.jpg',
+    title: 'Goodbye',
+    artist: [
+      'G.E.M.'
+    ]
+  }
+]
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <MusicPlayer playlist={playlist} autoplay />
       </div>
     );
   }
