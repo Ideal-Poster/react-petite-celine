3c8e427d77e4b1ae109d9801613f7ce46c74d9e0
Malcolm Gourdine <malcolmgourdine@gmail.com>
2019-01-30 15:51:30 -0500

    replace photoswipe with react-photo-gallery

--- src/art-page/ArtPage.js
+++ src/art-page/ArtPage.js
@@ -1,36 +1,116 @@
 import React, { Component } from 'react';
 import './ArtPage.css';
 
-import 'react-photoswipe/lib/photoswipe.css';
-import {PhotoSwipeGallery} from 'react-photoswipe';
+
 import { Col, Button } from 'antd';
 import items from './GalleryApi';
 import NavMenu from '../nav-menu/NavMenu';
+
+import Gallery from 'react-photo-gallery';
+import Lightbox from 'react-images';
+
+
 
 
 let options = {
   //http://photoswipe.com/documentation/options.html
 };
 
-class ArtPage extends Component {
-  getThumbnailContent = (item) => {
-    return (
-      <img rel="external" alt="Petite Celine artwork" src={item.thumbnail}/>
-    );
+const photos = [
+  {
+    src: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
+    width: 1,
+    height: 1
+  },
+  {
+    src: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
+    width: 3,
+    height: 4
+  },
+  {
+    src: "https://source.unsplash.com/iecJiKe_RNg/600x799",
+    width: 3,
+    height: 4
+  },
+  {
+    src: "https://source.unsplash.com/epcsn8Ed8kY/600x799",
+    width: 3,
+    height: 4
+  },
+  {
+    src: "https://source.unsplash.com/NQSWvyVRIJk/800x599",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "https://source.unsplash.com/zh7GEuORbUw/600x799",
+    width: 3,
+    height: 4
+  },
+  {
+    src: "https://source.unsplash.com/PpOHJezOalU/800x599",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "https://source.unsplash.com/I1ASdgphUH4/800x599",
+    width: 4,
+    height: 3
   }
-
+];
+class ArtPage extends Component {
+  constructor() {
+    super();
+    this.state = { currentImage: 0 };
+    this.closeLightbox = this.closeLightbox.bind(this);
+    this.openLightbox = this.openLightbox.bind(this);
+    this.gotoNext = this.gotoNext.bind(this);
+    this.gotoPrevious = this.gotoPrevious.bind(this);
+  }
+  openLightbox(event, obj) {
+    this.setState({
+      currentImage: obj.index,
+      lightboxIsOpen: true,
+    });
+  }
+  closeLightbox() {
+    this.setState({
+      currentImage: 0,
+      lightboxIsOpen: false,
+    });
+  }
+  gotoPrevious() {
+    this.setState({
+      currentImage: this.state.currentImage - 1,
+    });
+  }
+  gotoNext() {
+    this.setState({
+      currentImage: this.state.currentImage + 1,
+    });
+  }
   render() {
     return (
       <div>
-      {/* <div className="art-color-underlay"/> */}
-        <Col sm={{ span: 8, offset: 1 }} xl={{ span: 7, offset: 2 }}>
+      <div className="art-color-underlay"/>
+      <Col sm={{ span: 8, offset: 1 }} xl={{ span: 7, offset: 2 }}>
           <NavMenu/>
         </Col>
-
         <Col xs={24} md={{span: 15 }} style={{zIndex: 1000}}>
           <div className='background-color'/>
-          <Button className="etsy-button" type="primary" size="large">Etsy Store</Button>
-          <PhotoSwipeGallery items={items} options={options} thumbnailContent={this.getThumbnailContent}/>
+          <Gallery photos={photos} onClick={this.openLightbox} />
+          <Lightbox images={photos}
+            onClose={this.closeLightbox}
+            onClickPrev={this.gotoPrevious}
+            onClickNext={this.gotoNext}
+            currentImage={this.state.currentImage}
+            isOpen={this.state.lightboxIsOpen}
+          />
         </Col>
       </div>
     )
