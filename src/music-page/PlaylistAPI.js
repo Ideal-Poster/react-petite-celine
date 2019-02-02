7bc1a12060e19dbea75a611ca5ff3172c640d616
Malcolm Gourdine <malcolmgourdine@gmail.com>
2019-02-02 17:48:24 -0500

    event section styled

--- src/music-page/PlaylistAPI.js
+++ src/music-page/PlaylistAPI.js
@@ -1,45 +1,23 @@
 const playlist = [
   {
     url: 'http://res.cloudinary.com/alick/video/upload/v1502689683/Luis_Fonsi_-_Despacito_ft._Daddy_Yankee_uyvqw9.mp3',
-    cover: 'http://res.cloudinary.com/alick/image/upload/v1502689731/Despacito_uvolhp.jpg',
-    title: 'Despacito',
-    artist: [
-      'Luis Fonsi',
-      'Daddy Yankee'
-    ]
+    title: 'Despacito'
   },
   {
     url: 'http://res.cloudinary.com/alick/video/upload/v1502375674/Bedtime_Stories.mp3',
-    cover: 'http://res.cloudinary.com/alick/image/upload/v1502375978/bedtime_stories_bywggz.jpg',
-    title: 'Bedtime Stories',
-    artist: [
-      'Jay Chou'
-    ]
+    title: 'Bedtime Stories'
   },
   {
     url: 'http://res.cloudinary.com/alick/video/upload/v1502444212/Actor_ud8ccw.mp3',
-    cover: 'http://res.cloudinary.com/alick/image/upload/v1502444304/actor_umzdur.jpg',
-    title: '演员',
-    artist: [
-      '薛之谦'
-    ]
+    title: '演员'
   },
   {
     url: 'http://res.cloudinary.com/alick/video/upload/v1502444215/Bridge_of_Fate_aaksg1.mp3',
-    cover: 'http://res.cloudinary.com/alick/image/upload/v1502444306/Bridge_of_Fate_o36rem.jpg',
-    title: 'Bridge of Fate',
-    artist: [
-      '王力宏',
-      '谭维维'
-    ]
+    title: 'Bridge of Fate'
   },
   {
     url: 'http://res.cloudinary.com/alick/video/upload/v1502444222/Goodbye_byaom5.mp3',
-    cover: 'http://res.cloudinary.com/alick/image/upload/v1502444310/Goodbye_hpubmk.jpg',
-    title: 'Goodbye',
-    artist: [
-      'G.E.M.'
-    ]
+    title: 'Goodbye'
   }
 ]
 export default playlist;
