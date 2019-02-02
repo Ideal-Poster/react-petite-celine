eb65f3a472adda3174a6fe1101fc923056503195
Malcolm Gourdine <malcolmgourdine@gmail.com>
2019-02-02 17:09:05 -0500

    event section styled

--- src/events/Events.js
+++ src/events/Events.js
@@ -1,7 +1,7 @@
 import React from "react";
 import './Events.css';
 
-import { Col, Row } from 'antd';
+import { Col, Row, Button } from 'antd';
 
 const Events = () => (
   <div style={{border: "10px solid #1C1820"}}>
@@ -10,8 +10,40 @@ const Events = () => (
         <Col sm={{ offset: 2 }} md={{ offset:0,  span: 6 }}>
           <h3 className="events-banner">EVENTS</h3>
         </Col>
-        <Col span={16} style={{ background: 'grey' }}>
-          <p>hello</p>
+
+        <Col xs={{ span: 24 }} sm={{ span: 22, offset: 2 }} md={{span:18, offset:0}}>
+          <Row style={{ display: 'flex', paddingBottom: '40px' }}>
+            <Col xs={24} sm={16}>
+              <div className="event event01">
+                <div className="date">
+                  <p>Sat, January 19th 2019 - 10pm</p>
+                </div>
+                <div className="venue">
+                  <p>The Delancey</p>
+                </div>
+                <div className="location">
+                  <p>168 Delancey St New York, NY 10002 - $10 Cover Charge</p>
+                </div>
+              </div>
+            </Col>
+            <Col xs={24} sm={8}>
+              <div className="vertical-center">
+                <a href="">
+                  <Button
+                    className="button"
+                    size={'large'}>
+                    Primary
+                  </Button>
+                </a>
+              </div>
+            </Col>
+          </Row>
+
+          <Button
+            className="mobile-button"
+            size={'large'}>
+            Primary
+          </Button>
         </Col>
       </Row>
     </div>
